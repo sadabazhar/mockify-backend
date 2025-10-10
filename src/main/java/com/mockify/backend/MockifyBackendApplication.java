3d5664ef2e26746870eac8c1a26b2fde5968b7e8
Arshad Jamal <codingspidy@gmail.com>
2025-10-10 16:12:59 +0530

    initial commit

---
+++ src/main/java/com/mockify/backend/MockifyBackendApplication.java
@@ -0,0 +1,13 @@
+package com.mockify.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MockifyBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MockifyBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mockify/backend/MockifyBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mockify.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MockifyBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
