5e1dd796d0dafc7739c57c143521b39802fedd03
Arshad Jamal <codingspidy@gmail.com>
2025-10-30 11:39:22 +0530

    add API endpoints structure

---
+++ src/main/java/com/mockify/backend/controller/OrganizationController.java
@@ -0,0 +1,17 @@
+package com.mockify.backend.controller;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1/organizations")
+public class OrganizationController {
+    @GetMapping
+
+    @GetMapping("/{orgId}")
+
+    @PostMapping
+
+    @PutMapping("/{orgId}")
+
+    @DeleteMapping("/{orgId}")
+}
---
+++ src/main/java/com/mockify/backend/controller/PublicMockApiController.java
@@ -0,0 +1,11 @@
+package com.mockify.backend.controller;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1/mock")
+public class PublicMockApiController {
+    @GetMapping("/{schemaId}/records")
+
+    @GetMapping("/{schemaId}/records/{recordId}")
+}
