d8c2ce3e6d86fa4f62bde523ca7769389cb4775a
wasif-raza <wasifraza799@gmail.com>
2025-11-03 15:20:41 +0530

    user service implementation

--- src/main/java/com/mockify/backend/service/impl/UserServiceImpl.java
+++ src/main/java/com/mockify/backend/service/impl/UserServiceImpl.java
@@ -1,49 +1,80 @@
 package com.mockify.backend.service.impl;
 
 import com.mockify.backend.dto.response.auth.UserResponse;
+import com.mockify.backend.exception.ResourceNotFoundException;
+import com.mockify.backend.model.User;
+import com.mockify.backend.repository.UserRepository;
 import com.mockify.backend.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 //Manages users (typically admin-level operations).
 @Service
 public class UserServiceImpl implements UserService {
-
+    @Autowired
+    private  UserRepository userRepository;
     // Fetch user details by ID
     @Override
     public UserResponse getUserById(Long id) {
-        return null;
+        User user = userRepository.findById(id)
+                .orElseThrow(() ->
+                        new ResourceNotFoundException("User not found with ID: " + id));
+        return mapToResponse(user);
     }
 
     // Fetch user details by email
     @Override
     public UserResponse getUserByEmail(String email) {
-        return null;
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() ->
+                        new ResourceNotFoundException("User not found with email: " + email));
+        return mapToResponse(user);
     }
 
     // Get list of all users (admin use only)
     @Override
     public List<UserResponse> getAllUsers() {
-        return List.of();
+
+        return userRepository.findAll()
+                .stream()
+                .map(this::mapToResponse)
+                .collect(Collectors.toList())
     }
 
     // Delete user by ID
     @Override
     public void deleteUser(Long id) {
+        if (!userRepository.existsById(id)) {
+            throw new ResourceNotFoundException("User not found with ID: " + id);
+        }
+        userRepository.deleteById(id);
 
     }
 
     // Check if email already exists
     @Override
     public boolean existsByEmail(String email) {
-        return false;
+
+        return userRepository.existsByEmail(email);
     }
 
     // Total registered users
     @Override
     public long countUsers() {
-        return 0;
+
+        return userRepository.count();
+
+    }
+    private UserResponse mapToResponse(User user) {
+        return UserResponse.builder()
+                .id(user.getId())
+                .name(user.getName())
+                .email(user.getEmail())
+                .createdAt(user.getCreatedAt())
+                .build();
     }
 
 }
