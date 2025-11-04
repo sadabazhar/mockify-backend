8064851cd2e63906bcdc735e6fd217e9d1cb2388
Arshad Jamal <codingspidy@gmail.com>
2025-11-04 10:14:47 +0530

    ignore unimplemented controllers

--- src/main/java/com/mockify/backend/controller/PublicMockApiController.java
+++ src/main/java/com/mockify/backend/controller/PublicMockApiController.java
@@ -1,11 +1,11 @@
-package com.mockify.backend.controller;
-
-import org.springframework.web.bind.annotation.*;
-
-@RestController
-@RequestMapping("/api/v1/mock")
-public class PublicMockApiController {
-    @GetMapping("/{schemaId}/records")
-
-    @GetMapping("/{schemaId}/records/{recordId}")
-}
+//package com.mockify.backend.controller;
+//
+//import org.springframework.web.bind.annotation.*;
+//
+//@RestController
+//@RequestMapping("/api/v1/mock")
+//public class PublicMockApiController {
+//    @GetMapping("/{schemaId}/records")
+//
+//    @GetMapping("/{schemaId}/records/{recordId}")
+//}
