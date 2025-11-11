bdb9a3188c70fc3c0cb31a2f9a9351f64268049c
Arshad Jamal <codingspidy@gmail.com>
2025-11-11 21:14:16 +0530

    refactor(config): migrate to YAML config w/ env-based structure; add local postgres setup w/ Docker

--- src/main/java/com/mockify/backend/MockifyBackendApplication.java
+++ src/main/java/com/mockify/backend/MockifyBackendApplication.java
@@ -19,7 +19,6 @@ public class MockifyBackendApplication {
 		System.setProperty("DB_URL", dotenv.get("DB_URL"));
 		System.setProperty("DB_USER", dotenv.get("DB_USER"));
 		System.setProperty("DB_PASS", dotenv.get("DB_PASS"));
-		System.setProperty("DB_NAME", dotenv.get("DB_NAME"));
 
 		// Inject JWT credentials from .env
 		System.setProperty("JWT_SECRET", dotenv.get("JWT_SECRET"));
